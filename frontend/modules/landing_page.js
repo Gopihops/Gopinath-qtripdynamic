0cdcf1636e1f2b1b3b205dff3cba1c61502aadb9
CrioUser <crio-user@criodo.com>
2023-07-24 02:03:48 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS

--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,26 +1,26 @@
-import config from "../conf/index.js";
-
 async function init() {
   //Fetches list of all cities along with their images and description
+
   let cities = await fetchCities();
 
   //Updates the DOM with the cities
-  if (cities) {
-    cities.forEach((key) => {
-      addCityToDOM(key.id, key.city, key.description, key.image);
-    });
-  }
-}
 
 //Implementation of fetch call
 async function fetchCities() {
-  // TODO: MODULE_CITIES
-  // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    // TODO: MODULE_CITIES
+    // 1. Fetch cities using the Backend API and return the data
+    const cities = await fetch(config.backendEndpoint + "/cities");
+    const data = await cities.json();
+    console.log(cities);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
-function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
 
@@ -44,6 +44,5 @@ function addCityToDOM(id, city, description, image) {
 }
 
 
-
-
 export { init, fetchCities, addCityToDOM };
+
